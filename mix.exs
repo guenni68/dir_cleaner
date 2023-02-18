c6ce9150bb6137d93daa91e415579d68705000fa
Guenther Schmidt <schmig68@gmail.com>
2023-02-18 23:43:25 +0000

    config added

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule DirCleaner.MixProject do
   use Mix.Project
 
+  @version File.read!("VERSION") |> String.trim()
+
   def project do
     [
       app: :dir_cleaner,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
