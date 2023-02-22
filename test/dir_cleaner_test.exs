e26368902f6f1d0f2f7f1cefe93d0ec413d4ca26
Guenther Schmidt <schmig68@gmail.com>
2023-02-22 10:49:03 +0000

    added config test

--- test/dir_cleaner_test.exs
+++ test/dir_cleaner_test.exs
@@ -2,5 +2,10 @@ defmodule DirCleanerTest do
   use ExUnit.Case
 
   test "verify config" do
+    assert DirCleaner.Config.directories() == ["/tmp/dummy"]
+    assert [%{start: {DirCleaner.Worker, _, _}}] = DirCleaner.Config.child_specs()
+    assert DirCleaner.Config.max_age() == :timer.minutes(10)
+    assert DirCleaner.Config.interval() == :timer.minutes(10)
+    assert DirCleaner.Config.mode() == :touched
   end
 end
